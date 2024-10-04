91e2d16f989eef1bf00664baa3fc47447692a5c8
thanhhoa0000 <109255962+thanhhoa0000@users.noreply.github.com>
2024-10-04 06:41:58 +0000

    Add Task.API endpoints

--- src/Task.API/Endpoints/TaskItemEndpoints.cs
+++ src/Task.API/Endpoints/TaskItemEndpoints.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Task.API.Endpoints;
 
@@ -10,6 +11,11 @@ public class TaskItemEndpoints : ICarterModule
 
         group.MapGet("/", GetAllTasks);
         group.MapGet("/{id:guid}", GetAllTasks);
+
+        group.MapPost("/", AddTask);
+        group.MapPut("/", UpdateTask);
+
+        group.MapDelete("/{id:guid}", DeleteTask);
     }
 
     public static async Task<IResult> GetAllTasks(TaskContext context, IMapper mapper)
@@ -19,13 +25,76 @@ public class TaskItemEndpoints : ICarterModule
         return TypedResults.Ok(mapper.Map<IEnumerable<TaskItemDto>>(tasks));
     }
 
-    public static async Task<Results<Ok<TaskItemDto>, BadRequest<string>>> GetTaskById(TaskContext context, IMapper mapper, Guid id)
+    public static async Task<Results<Ok<TaskItemDto>, BadRequest<string>>> GetTaskById(
+        TaskContext context,
+        IMapper mapper,
+        Guid id)
     {
         try
         {
             var task = await context.TaskItems.FindAsync(id);
 
             return TypedResults.Ok(mapper.Map<TaskItemDto>(task));
+        }
+        catch (Exception ex)
+        {
+            return TypedResults.BadRequest(ex.Message);
+        }
+    }
+
+    public static async Task<Results<Ok<string>, BadRequest<string>>> AddTask(
+        TaskContext context,
+        [FromBody] IMapper mapper,
+        TaskItemDto taskDto)
+    {
+        try
+        {
+            var task = mapper.Map<TaskItem>(taskDto);
+
+            await context.TaskItems.AddAsync(task);
+            await context.SaveChangesAsync();
+
+            return TypedResults.Ok("Task added successfully!");
+        }
+        catch (Exception ex)
+        {
+            return TypedResults.BadRequest(ex.Message);
+        }
+    }
+
+    public static async Task<Results<Ok<string>, BadRequest<string>>> UpdateTask(
+        TaskContext context,
+        [FromBody] IMapper mapper,
+        TaskItemDto taskDto)
+    {
+        try
+        {
+            var task = mapper.Map<TaskItem>(taskDto);
+
+            context.TaskItems.Update(task);
+            await context.SaveChangesAsync();
+
+            return TypedResults.Ok("Task updated successfully!");
+        }
+        catch (Exception ex)
+        {
+            return TypedResults.BadRequest(ex.Message);
+        }
+    }
+
+    public static async Task<Results<Ok<string>, BadRequest<string>>> DeleteTask(
+        TaskContext context,
+        IMapper mapper,
+        Guid id)
+    {
+        try
+        {
+            var task = await context.TaskItems.FindAsync(id);
+
+            context.TaskItems.Remove(task);
+            await context.SaveChangesAsync();
+
+            return TypedResults.Ok("Task deleted successfully!");
         }
         catch (Exception ex)
         {
